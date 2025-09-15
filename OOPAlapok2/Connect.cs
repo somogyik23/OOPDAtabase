db1fb91855ed8c7c8dd2bd3f6913d79d4e1867fe
Somogyik <Somogyik>
2025-09-15 12:25:44 +0200

    elso

---
+++ OOPAlapok2/Connect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAlapok2
+{
+    internal class Connect
+    {
+        private string Host;
+        private string database;
+        private string user;
+        private string Password;    
+    
+
+    public Connect(string database, string user, string password)
+        {
+            this.Host = "localhost";
+            this.database = database;
+            this.user = user;
+            this.Password = password;
+
+            ConnectionString = $"Server={_host};Database={database};Uid={user};Pwd={password};";
+
+            connectionStirng = new MySqlConnection(ConnectionString);
+      
+        }
+    }
+}
---
+++ OOPAlapok2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace OOPAlapok2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adja meg az adatbázis nevét: ");
+            string database = Console.ReadLine();
+
+            Console.Write("Felhasználónév: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Jelszó: ");
+            string password = ReadPassword();
+
+            string connectionString = $"Server=localhost;Database={database};Uid={username};Pwd={password};";
+
+            try
+            {
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Sikeres csatlakozás");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Sikertelen csatlakozás: {ex.Message}");
+            }
+        }
+
+        // Secure password input (does not show password on console)
+        static string ReadPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo info;
+            do
+            {
+                info = Console.ReadKey(true);
+                if (info.Key != ConsoleKey.Enter)
+                {
+                    password += info.KeyChar;
+                }
+            } while (info.Key != ConsoleKey.Enter);
+            Console.WriteLine();
+            return password;
+        }
+    }
+}
