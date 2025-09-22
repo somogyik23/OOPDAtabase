4678f081d5269f720b447b07456d6cbac078b2a9
Somogyik <Somogyik>
2025-09-22 08:09:14 +0200

    Interface es teblebooks hozzaadodtak

---
+++ OOPAlapok2/Services/TableBooks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAlapok2.Services
+{
+    internal class TableBooks : ISqlStatements
+    {
+        public List<object> GetAllBooks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
